24bee9b558690a3c1388a15d51596b6b5de248d2
Mahmoud Abd alaziz <mahmoud.a.ali.88@gmail.com>
2023-01-30 03:48:19 +0200

    lets Go

---
+++ my-app/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Button />
+      </header>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div>
+      <button id="button">Load</button>
+      <p>Hello React!</p>
+    </div>
+  );
+}
+
+export default App;
+
